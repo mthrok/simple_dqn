a55d45f53cb03516a686ca3e7144158f15705250
moto  (´・ω・`) <mthrok@gmail.com>
2016-07-05 11:49:41 -0700

    Move target Q value conputation inside TF

--- src/tf_model.py
+++ src/tf_model.py
@@ -164,3 +164,102 @@ class L2(object):
                            name='L2_error')
         self.error = tf.reduce_mean(error, name='mean_L2_error')
         return self.error
+
+
+def vanilla_dqn(num_actions, data_format):
+    initializer = tf.random_normal_initializer(mean=0.0, stddev=0.01)
+    conv1 = Conv2D((8, 8, 32), 4, initializer, 'VALID', data_format)
+    conv2 = Conv2D((4, 4, 64), 2, initializer, 'VALID', data_format)
+    conv3 = Conv2D((3, 3, 64), 1, initializer, 'VALID', data_format)
+    dense1 = Dense(512, initializer)
+    dense2 = Dense(num_actions, initializer)
+
+    model = Model()
+    model.add_layer(TrueDiv(255.0), 'layer0/normalization')
+
+    model.add_layer(conv1, 'layer1/conv2D')
+    model.add_layer(ReLU(), 'layer1/ReLU')
+
+    model.add_layer(conv2, 'layer2/conv2D')
+    model.add_layer(ReLU(), 'layer2/ReLU')
+
+    model.add_layer(conv3, 'layer3/conv2D')
+    model.add_layer(ReLU(), 'layer3/ReLU')
+
+    model.add_layer(Flatten(), 'layer4/flatten')
+
+    model.add_layer(dense1, 'layer5/dense')
+    model.add_layer(ReLU(), 'layer5/ReLU')
+
+    model.add_layer(dense2, 'layer6/dense')
+    return model
+
+
+class QLearning(object):
+    def __init__(self, model_factory, input_shape, discount_rate,
+                 min_reward=None, max_reward=None, datatype='float32'):
+        self.input_shape = input_shape
+        self.discount_rate = discount_rate
+        self.datatype = datatype
+        self.min_reward = min_reward
+        self.max_reward = max_reward
+
+        self._build_network(model_factory)
+        self._build_sync_ops()
+
+    def _build_network(self, model_factory):
+        with tf.variable_scope('pre_trans'):
+            self.prestates = tf.placeholder(
+                dtype=self.datatype, shape=self.input_shape, name='prestates')
+            self.pre_trans_model = model_factory()
+            self.pre_trans_model(self.prestates)
+        with tf.variable_scope('post_trans'):
+            self.poststates = tf.placeholder(
+                dtype=self.datatype, shape=self.input_shape, name='poststates')
+            self.post_trans_model = model_factory()
+            self.post_trans_model(self.poststates)
+        with tf.name_scope('q_value'):
+            self._build_q_value()
+
+    def _build_q_value(self):
+        self.actions = tf.placeholder(
+            dtype='int32', shape=(None,), name='actions')
+        self.rewards = tf.placeholder(
+            dtype=self.datatype, shape=(None,), name='rewards')
+        self.terminals = tf.placeholder(
+            dtype=self.datatype, shape=(None,), name='terminals')
+        with tf.name_scope('future_rewrad'):
+            post_q = tf.reduce_max(self.post_trans_model.output_tensor,
+                                   reduction_indices=1, name='max_post_q')
+            post_q = tf.mul(self.discount_rate, post_q,
+                            name='discounted_max_post_q')
+            post_q = tf.mul((1.0 - self.terminals), post_q,
+                            name='masked_max_post_q')
+            rewards = self.rewards
+            if self.min_reward and self.max_reward:
+                rewards = tf.clip_by_value(
+                    rewards, self.min_reward,
+                    self.max_reward, name='clipped_reward')
+            future_reward = tf.add(rewards, post_q, name='future_reward')
+
+        with tf.name_scope('target_q_value'):
+            target = tf.identity(self.pre_trans_model.output_tensor)
+            n_actions = target.get_shape().as_list()[1]
+
+            future_reward = tf.reshape(future_reward, (-1, 1))
+            future_reward = tf.tile(future_reward, tf.pack([1, n_actions]))
+
+            mask_on = tf.one_hot(
+                self.actions, depth=n_actions, on_value=1., off_value=0.)
+            mask_off = tf.one_hot(
+                self.actions, depth=n_actions, on_value=0., off_value=1.)
+
+            self.target_q = tf.add(target * mask_off, future_reward * mask_on,
+                                   name='target_q')
+
+    def _build_sync_ops(self):
+        src_vars = self.pre_trans_model.get_variables().values()
+        tgt_vars = self.post_trans_model.get_variables().values()
+        with tf.name_scope('sync'):
+            ops = [tgt.assign(src) for src, tgt in zip(src_vars, tgt_vars)]
+            self.sync_op = tf.group(*ops, name='sync')
